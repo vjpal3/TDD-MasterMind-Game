9c621870fc5e4eb6b98d60361eb50ca776ccaec1
vjpal3 <vrishalipal@yahoo.com>
2020-02-21 13:37:25 -0500

    Refactor tests to use TestCase attribute

--- TDDMasterMindGame.Tests/GameTests.cs
+++ TDDMasterMindGame.Tests/GameTests.cs
@@ -16,6 +16,7 @@ namespace TDDMasterMindGame.Tests
        public void Setup()
         {
             game = new Game(new CodeGenerator(), new GameStatus(), new GameInputValidator());
+            game.code = new int[] { 1, 2, 3, 4 };
         }
 
         [TearDown]
@@ -24,44 +25,25 @@ namespace TDDMasterMindGame.Tests
             game = null;
         }
         
+        [TestCase(new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new int[] { 1, 2, 3 })]
 
-        [Test]
-        public void ThrowExceptionIfAttemptLenghtIsMoreThan4()
+        public void ThrowExceptionIfAttemptLenghtIsMoreThan4OrLessThan4(int[] input)
         {
-            game.code = new int[] { 1, 2, 3, 4 };
-            var input = new int[] { 1, 2, 3, 4, 5 };
             Assert.Throws<System.ArgumentException>(() => game.CheckScore(input));
         }
 
-        [Test]
-        public void ThrowExceptionIfAttemptLengthIsLessThan4()
+        [TestCase(new int[] { 1, 2, 3, 7 })]
+        [TestCase(new int[] { 0, 2, 3, 4 })]
+        public void ThrowExceptionIfNumberInAttemptArrayIsGreaterThan6OrLessThan1(int[] input)
         {
-            game.code = new int[] { 1, 2, 3, 4 };
-            var input = new int[] { 1, 2, 3 };
-            Assert.Throws<System.ArgumentException>(() => game.CheckScore(input));
-        }
-
-        [Test]
-        public void ThrowExceptionIfNumberInAttemptArrayIsGreaterThan6()
-        {
-            game.code = new int[] { 1, 2, 3, 4 };
-            var input = new int[] { 1, 2, 3, 7 };
             Assert.Throws<System.ArgumentOutOfRangeException>(() => game.CheckScore(input));
 
         }
 
         [Test]
-        public void ThrowExceptionIfNumberInAttemptArrayIsLessThan1()
-        {
-            game.code = new int[] { 1, 2, 3, 4 };
-            var input = new int[] { 0, 2, 3, 4 };
-            Assert.Throws<ArgumentOutOfRangeException>(() => game.CheckScore(input));
-        }
-
-        [Test]
         public void ThrowExceptionIfAllNumbersInAttemptArrayAreNotUnique()
         {
-            game.code = new int[] { 1, 2, 3, 4 };
             var input = new int[] { 1, 2, 2, 4 };
 
             Assert.Throws<Exception>(() => game.CheckScore(input));
@@ -71,7 +53,6 @@ namespace TDDMasterMindGame.Tests
         [Test]
         public void IfGameIsWon_ReturnGameStatusIsWonTrue()
         {
-            game.code = new int[] { 1, 2, 3, 4 };
             var input = new int[] { 1, 2, 3, 4 };
 
             var result = game.CheckScore(input);
@@ -83,7 +64,6 @@ namespace TDDMasterMindGame.Tests
         [Test]
         public void If3NumbersAreCorrect_ReturnGameStatusCorrectNumsIs3()
         {
-            game.code = new int[] { 1, 2, 3, 4 };
             var input = new int[] { 1, 2, 5, 3 };
 
             var result = game.CheckScore(input);
@@ -95,11 +75,21 @@ namespace TDDMasterMindGame.Tests
         [Test]
         public void If2NumbersAreInCorrectPosition_ReturnGameStatusCorrectPositionsIs2()
         {
-            game.code = new int[] { 1, 2, 3, 4 };
-            var input = new int[] { 1, 2, 5, 3 };
+            var input = new int[] { 1, 2, 3, 5 };
 
             var result = game.CheckScore(input);
-            var expectedResult = "Game Status: \nCorrect Numbers: 3\nCorrect Positions: 2";
+            var expectedResult = "Game Status: \nCorrect Numbers: 3\nCorrect Positions: 3";
+            Assert.That(result, Is.EqualTo(expectedResult));
+
+        }
+
+        [Test]
+        public void CorrectNumbersShouldIgnoreCorrectPositions()
+        {
+            var input = new int[] { 1, 4, 5, 3 };
+
+            var result = game.CheckScore(input);
+            var expectedResult = "Game Status: \nCorrect Numbers: 3\nCorrect Positions: 1";
             Assert.That(result, Is.EqualTo(expectedResult));
 
         }
